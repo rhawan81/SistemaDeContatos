eb4dcef3cac0f7e3c6f75158e954f5339d105a4a
Michel <rhawanjs@gmail.com>
2026-03-19 22:31:07 -0300

    add contact listing, searching, and deletion methods to contact manager

--- src/br/com/contatos/gerenciador/GerenciadorContatos.java
+++ src/br/com/contatos/gerenciador/GerenciadorContatos.java
@@ -9,10 +9,42 @@ public class GerenciadorContatos {
     private ArrayList<Contato> contatos = new ArrayList<>();
 
     public void adicionarContato(String nome, String telefone, String email){
-        // Cria um Objeto  Contato usando todos os dados que chegaram como parametro no metodo
+        // Cria um Objeto Contato usando todos os dados que chegaram como parametro no metodo
         Contato novo = new Contato(nome, telefone, email);
-        contatos.add(novo);
+        contatos.add(novo); // adiciona o objeto criado a o array que recebe os parametros.
         System.out.println("Contato adicionado com sucesso !!");
     }
+
+    public void listarContatos(){
+        if(contatos.isEmpty()){ // metodo que verifica se o array esta vazio , se estiver retornar true caso nao retornar falso e pula o if
+            System.out.println("Nenhum contato cadastrado");
+            return;
+        }
+        System.out.println("----- Lista de Contatos -----");
+        for (int i = 0; i < contatos.size() ; i++) { ///  size metodo que conta quantos contatos tem na lista se tiver 3 contatos ira imprimir.
+            // o loop ira percorrer caso tenha 3 contatos 0,1,2;
+            System.out.println("[ " + i + "] "+ contatos.get(i));
+
+        }
+    }
+
+    public Contato buscarContatos(String nome){
+        for(Contato c : contatos){
+            // pega o nome do contato da lista e compara com o nome digitado pelo usuário
+            if (c.getNome().equalsIgnoreCase(nome)){
+                // se achar ele retorna esse contato
+                return c;
+            }
+        }
+        // se não achou devolve vazia
+        return null;
+    }
+    public Contato excluirContato(String nome){
+        if
+    }
+
+
+
+
 
 }
